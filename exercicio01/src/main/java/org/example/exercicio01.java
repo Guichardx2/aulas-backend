2bfe0c533494254f9af15560c13fbfe3b8ee5ce9
Guichard² <jeancharles.g.guichard@gmail.com>
2024-08-15 15:53:50 -0300

    exercicio01

---
+++ exercicio01/src/main/java/org/example/exercicio01.java
@@ -0,0 +1,66 @@
+package org.example;
+
+import java.util.Objects;
+import java.util.Scanner;
+import java.util.Random;
+
+public class exercicio01 {
+    public static void main(String[] args) {
+        while(true){
+            String op1;
+            String op2;
+
+            // Array de filmes de Sci-fi Drama
+            String[] sciFiDrama = {"Interstellar", "Arrival", "Ex Machina", "Her", "2001: Uma Odisseia no Espaço"};
+
+            // Array de filmes de Sci-fi Comédia
+            String[] sciFiComedia = {"Galaxy Quest", "O Quinto Elemento", "Back to the Future", "Men in Black", "Spaceballs"};
+
+            // Array de filmes Medieval Comédia
+            String[] medievalComedia = {"Monty Python e o Cálice Sagrado", "A Princesa Prometida", "O Nome da Rosa", "Shrek", "Alexandre e o Dia Maravilhoso"};
+
+            // Array de filmes Medieval Drama
+            String[] medievalDrama = {"Gladiador", "O Último dos Moicanos", "O Nome da Rosa", "Braveheart", "A 13ª Guerreira"};
+
+            Random aleatorio= new Random();
+
+            //array de Sci-fi Drama
+            int posicaoAleatoriaScifiDrama = aleatorio.nextInt(sciFiDrama.length);
+            String scifiDramaEscolhido = sciFiDrama[posicaoAleatoriaScifiDrama];
+
+            //array de Sci-fi Comédia
+            int posicaoAleatoriaScifiComedia = aleatorio.nextInt(sciFiComedia.length);
+            String sciFiComediaEscolhido =  sciFiComedia[posicaoAleatoriaScifiComedia];
+
+            //Array de Medieval Comédia
+            int posicaoAleatoriaMedComedia = aleatorio.nextInt(medievalComedia.length);
+            String medComediaEscolhido =  medievalComedia[posicaoAleatoriaMedComedia];
+
+            //Array de Medieval Drama
+            int posicaoAleatoriaMedDrama = aleatorio.nextInt(medievalDrama.length);
+            String medDramaEscolhido= medievalDrama[posicaoAleatoriaMedDrama];
+
+            Scanner leitor = new Scanner(System.in);
+
+            System.out.println("Qual ambientação você prefere?");
+            System.out.println("1- Sci-fi\n" + "2- Medieval");
+            op1= leitor.nextLine();
+
+            System.out.println("Qual gênero você prefere?");
+            System.out.println("1-Comédia\n" + "2- Drama");
+            op2= leitor.nextLine();
+
+            if (op1.isEmpty() || op2.isEmpty()) return;
+
+            if (Objects.equals(op1, "1") && Objects.equals(op2, "1")){
+                System.out.println("Recomendamos este filme para você: "+ sciFiComediaEscolhido);
+            }else if (Objects.equals(op1, "1") && Objects.equals(op2, "2")){
+                System.out.println("Recomendamos este filme para você: "+ scifiDramaEscolhido);
+            }else if (Objects.equals(op1, "2") && Objects.equals(op2, "1")){
+                System.out.println("Recomendamos este filme para você: "+ medComediaEscolhido);
+            }else if (Objects.equals(op1, "2") && Objects.equals(op2, "2")){
+                System.out.println("Recomendamos este filme para você: "+ medDramaEscolhido);
+            }
+        }
+    }
+}
