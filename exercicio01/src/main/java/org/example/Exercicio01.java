24aeb5b521860fa5915e2fb9c0e2279e2a432a2b
Guichard² <jeancharles.g.guichard@gmail.com>
2024-08-25 21:48:21 -0300

    corrigindo erro IDE

--- exercicio01/src/main/java/org/example/Exercicio01.java
+++ exercicio01/src/main/java/org/example/Exercicio01.java
@@ -4,7 +4,7 @@ import java.util.Objects;
 import java.util.Scanner;
 import java.util.Random;
 
-public class exercicio01 {
+public class Exercicio01 {
     public static void main(String[] args) {
         while(true){
             String op1;
